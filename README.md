000997959e125c2a7795fe6c8e87db39b3ca999e
Willian Mascimiano <willian@MacBook-Air-de-Willian.local>
2021-02-11 02:33:28 -0300

    0.0.1

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Em caso de dúvidas [entre em contato](https://willian.eti.br/contato/)
 
 | Data de Criação | Documentação |
 | ------ | ------ |
+| 11/02/2021 | Alteração do nome Pesquisador para Explorer. |
+| 11/02/2021 | Unificação dos projetos no mesmo repositório e melhorado a separação dos arquivos. |
 | 03/10/2020 | [Pesquisador Fluig](https://willian.eti.br/pesquisador-generico-de-dados-de-processos-no-fluig/) |
 | 08/06/2020 | [Fórmula Visual RM e  Fluig](https://willian.eti.br/pesquisador-generico-de-dados-de-processos-no-fluig/) |
 | 06/05/2020 | [Anexar arquivos de Forma automática](https://willian.eti.br/como-inserir-anexos-no-processo-do-fuig-de-forma-automatica/) |
--- explorer/datasets/ds_configura_explorer.js
+++ explorer/datasets/ds_configura_explorer.js
@@ -7,33 +7,55 @@ function onSync(lastSyncDate) {
 }
 
 function createDataset(fields, constraints, sortFields) {
+	
+	
+	/**
+	 *  Créditos: 
+	 *  	Este código utiliza a estrutura de navegação em dataset pai x filho, gerado pelo SNIPPETS, plugin do fluig.
+	 * */
+	
+	
 
 	var processo = fields[0];
 
-	//Cria a constraint para buscar os formulários ativos
+	/**
+	 * O formulário frm_consulta_explorer é um configurador para permitir que seja
+	 * consultado qualquer dataset através deste explorer.
+	 * */
+	
+	
 	var c1 = DatasetFactory.createConstraint("metadata#active", true, true, ConstraintType.MUST);
 	var c2 = DatasetFactory.createConstraint("config_nome_dataset", processo, processo, ConstraintType.MUST);
 	var constraints = new Array(c1, c2);
 
 	// Consulta dataset do formulário que tem o elemento Pai x Filho
-	var datasetPrincipal = DatasetFactory.getDataset("DSConfiguraPesquisador", null, constraints, null);
+	
+	
+	
+	var datasetPrincipal = DatasetFactory.getDataset("ds_configura_explorer", null, constraints, null);
 	var resultados = [];
 	for (var i = 0; i < datasetPrincipal.rowsCount; i++) {
 		var documentId = datasetPrincipal.getValue(i, "metadata#id");
 		var documentVersion = datasetPrincipal.getValue(i, "metadata#version");
 
-		//Cria as constraints para buscar os campos filhos, passando o tablename, número da formulário e versão
-		var c1 = DatasetFactory.createConstraint("tablename", "ConfiguraPesquisadorItem", "ConfiguraPesquisadorItem", ConstraintType.MUST);
+		
+		/***
+		 * 
+		 * Criar as constraints para consultar os datasets para consultar os itens da tabela do formulário pai x filho.
+		 * 
+		 * */
+		var c1 = DatasetFactory.createConstraint("tablename", "configura_explorer_item", "configura_explorer_item", ConstraintType.MUST);
 		var c2 = DatasetFactory.createConstraint("metadata#id", documentId, documentId, ConstraintType.MUST);
 		var c3 = DatasetFactory.createConstraint("metadata#version", documentVersion, documentVersion, ConstraintType.MUST);
 		var constraintsFilhos = new Array(c1, c2, c3);
 
-		//Busca o dataset
-		var datasetFilhos = DatasetFactory.getDataset("DSConfiguraPesquisador", null, constraintsFilhos, null);
+		/**
+		 * Consultar no dataset do formulário os dados referentes à tabela de itens do configurador
+		 * 
+		 * */
+		var datasetFilhos = DatasetFactory.getDataset("ds_frm_configura_explorer", null, constraintsFilhos, null);
 		return datasetFilhos;
-		//	for (var j = 0; j < datasetFilhos.rowsCount; j++) {
-		//		datasetFilhos.getValue(j, "nome_campo_filho"),
-		//	}
+		 
 
 	}
 
